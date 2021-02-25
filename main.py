9724e8359ea8fd8d0c4b6a876292a01f2197db0b
Rafael Córdoba <contact@rcordoba.me>
2021-02-24 21:45:59 -0500

    Add reasoning as database check

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ from database import Database
 def sendPicks():
 	picks = Pick().getPicks('https://www.pickswise.com/nhl/best-bets/')
 	soup = BeautifulSoup(picks, 'lxml')
-	bets = soup.find_all('div', class_='PickPrediction__body')
+	divs = soup.find_all('div', class_='CardV2')
 
-	for bet in bets:
-		full_pick = Pick().parsePick(bet)
+	for bet in divs:
+		if not bet.find('div', class_='PickPrediction__pick-header'):
+			continue
+
+		pick = Pick().parsePick(bet)
 
 		connection = Database().connection()
-
-		# Get Cursor
 		cur = connection.cursor()
-		cur.execute("SELECT * FROM nhl WHERE pick=?", (full_pick,))
+		cur.execute("SELECT * FROM nhl WHERE reasoning=?", (pick['reasoning'],))
 		db_pick = cur.fetchall()
 
 		# Insert if pick doesnt exists
 		if not db_pick:
 			try:
-				db_insert = cur.execute("INSERT INTO nhl (pick) VALUES (?)", (full_pick,))
-				print('Sending message')
+				db_insert = cur.execute("INSERT INTO nhl (pick, reasoning) VALUES (?, ?)", (pick['full_pick'], pick['reasoning'],))
 			except mariadb.Error as e:
 				print(f"Error: {e}")
+
 			connection.commit()
 			connection.close()
 
-			TelegramBot().sendMessage(full_pick)
-			print('Pick sent')
+			TelegramBot().sendMessage(pick['full_pick'])
+			print('Message sent')
 		else:
 			print('Old pick')
 
-# schedule.every(30).minutes.do(sendPicks)
-schedule.every(2).seconds.do(sendPicks)
+schedule.every(30).minutes.do(sendPicks)
 
 while True:
 	schedule.run_pending()
--- pick.py
+++ pick.py
@@ -14,15 +14,22 @@ class Pick:
 		return response.content
 
 	def parsePick(self, data):
-		pick_category = data.find('div', class_='PickPrediction__pick-header').get_text().strip() # Money Line Pick
-		pick_event = re.findall(r'data-analytics-event-label=\"(.*)\" data-component', str(data))[0] # TOR Maple Leafs @ MON Canadiens
-		pick = data.find('div', class_='PickPrediction__pick-text').get_text().strip() # TOR Maple Leafs Win
-
-		if data.find('button', class_='PickPrediction__toggle-betslip'):
-			pick_result = data.find('button', class_='PickPrediction__toggle-betslip').get_text().strip().split("\n") # -6.5
+		if not data.find('div', class_='PickReasoning__content'):
+			return None
 		else:
-			pick_result = data.find('a', class_='PickPrediction__toggle-betslip').get_text().strip().split("\n") # -6.5
+			pick_category = data.find('div', class_='PickPrediction__pick-header').get_text().strip() # Money Line Pick
+			pick_event = re.findall(r'data-analytics-event-label=\"(.*)\" data-component', str(data))[0] # TOR Maple Leafs @ MON Canadiens
+			pick = data.find('div', class_='PickPrediction__pick-text').get_text().strip() # TOR Maple Leafs Win
+			pick_reasoning = data.find('div', class_='PickReasoning__content').get_text().strip() # Marner is second in the NHL with...
 
-		full_pick = pick_category + ' \n' + '<b>' + pick_event + '</b> \n' + '<pre>' + pick + '</pre>' +  ' ' + pick_result[0]
+			if data.find('button', class_='PickPrediction__toggle-betslip'):
+				pick_result = data.find('button', class_='PickPrediction__toggle-betslip').get_text().strip().split("\n") # -6.5
+			else:
+				pick_result = data.find('a', class_='PickPrediction__toggle-betslip').get_text().strip().split("\n") # -6.5
 
-		return full_pick
+			bet = {
+				"reasoning": pick_reasoning,
+				"full_pick": pick_category + ' \n' + '<b>' + pick_event + '</b> \n' + '<pre>' + pick + '</pre>' +  ' ' + pick_result[0]
+			}
+
+			return bet
